b6f203daf157bf274e2ace21a451859e00bc7cbb
toddmnortonCO <toddmnorton@live.com>
2020-11-16 16:33:01 -0700

    Initial Commit

---
+++ I Am Right/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Right
+//
+//  Created by Todd Norton on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
